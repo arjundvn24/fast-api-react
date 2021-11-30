51be426e556e18f4df430da9dee20528357d5c4e
arjun@DESKTOP-MG2DPEH <arjundhawan2002@gmail.com>
2021-11-30 23:08:25 +0530

    error1

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const port = process.env.port;
 const Routes = require("./clientnew/src/app/route");
 
 app.use([cors(), Routes]);
-app.use(express.static(path.join(__dirname, 'clientnew/build')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
